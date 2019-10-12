5fba43d393f844bd5ca44776a3f29a2362cc8df5
Gazzzolina <55655206+Gazzzolina@users.noreply.github.com>
2019-10-13 02:24:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function validateFormRna() {
     const chars = {'G':'C','C':'G','T':'A','A':'U'};
     rna = str.replace(/[GCTA]/g, m => chars[m]);
     document.getElementById('resultDna').innerHTML = `${rna}`;
-    }
+  }
 
   function rnaToDna () {
     let str = document.getElementById('rna').value;
